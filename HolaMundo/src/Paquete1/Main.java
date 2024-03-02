a1a02297d75ba87efafd155b50127a3c0b56538c
PC <PC@LORD-BITES.home>
2024-03-02 23:32:05 +0100

    commit inicial

---
+++ HolaMundo/src/Paquete1/Main.java
@@ -0,0 +1,10 @@
+package Paquete1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+		System.out.println("Hola mundo");
+	}
+
+}
