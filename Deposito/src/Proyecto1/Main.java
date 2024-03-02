96b511a6f7dd91f02354ae15aad9091e3c840637
PC <PC@LORD-BITES.home>
2024-03-02 20:24:01 +0100

    commit inicial

---
+++ Deposito/src/Proyecto1/Main.java
@@ -0,0 +1,27 @@
+package Proyecto1;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+
+		CCuenta miCuenta;
+        double saldoActual;
+
+        miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = miCuenta.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+
+        try {
+            miCuenta.retirar(2300);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            miCuenta.ingresar(695);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+	}
+
+}
