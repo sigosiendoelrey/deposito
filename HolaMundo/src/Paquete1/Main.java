30c193b661613a4bc0c4636e20cdaf507d0017ad
PC <PC@LORD-BITES.home>
2024-04-30 20:15:55 +0200

    commit

--- HolaMundo/src/Paquete1/Main.java
+++ HolaMundo/src/Paquete1/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Esbozo de método generado automáticamente
 		System.out.println("Hola mundo");
+		System.out.println("Sigo siendo elrey");
 	}
 
 }
